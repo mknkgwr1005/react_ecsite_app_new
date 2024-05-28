0392bdad4ed1181371c1f6ee035b882ab4b577d1
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2024-05-28 14:52:09 +0900

    注文履歴のテーブルのCSSを調整

--- src/pages/OrderHistory.tsx
+++ src/pages/OrderHistory.tsx
@@ -3,12 +3,35 @@ import { useEffect, useRef, useState } from "react";
 import { Order } from "../types/Order";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import "../css/orderHistory.css";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import TableHead from "@mui/material/TableHead";
 
 export const OrderHistory = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
   const currentUser = auth.currentUser;
   const currentUserUid = currentUser?.uid;
 
@@ -66,125 +89,136 @@ export const OrderHistory = () => {
   }, []);
 
   return (
-    <div>
-      <TableContainer>
-        <Table
-          sx={{ minWidth: 50, border: 2 }}
-          aria-label="spanning table"
-          padding="normal"
-        >
+    <TableContainer className="TableContainer" component={Paper}>
+      <Table aria-label="customized table" padding="normal">
+        <TableHead id="order-header">
           <TableRow>
-            <TableCell scope="col" id="order-date" align="center">
+            <StyledTableCell scope="col" id="order-date" align="center">
               注文日
-            </TableCell>
-            <TableCell scope="col" id="order-detail" align="center">
+            </StyledTableCell>
+            <StyledTableCell scope="col" id="order-detail" align="center">
               注文内容
-            </TableCell>
+            </StyledTableCell>
           </TableRow>
-
+        </TableHead>
+        <TableBody>
           {orderList.map((order, orderIndex) => (
             // 注文日
-            <TableRow key={orderIndex}>
-              <TableCell headers="order-date" scope="row">
+            <StyledTableRow className="per-order-info" key={orderIndex}>
+              <StyledTableCell
+                className="order-date"
+                scope="row"
+                component="th"
+              >
                 {order.deliveryTime}
-              </TableCell>
+              </StyledTableCell>
               {/* 注文内容 */}
               {order.orderItemList.map((itemInfo, itemIndex) => (
                 <>
-                  <TableRow>
-                    <TableCell id="item-name" scope="col" align="center">
+                  <TableRow className="order-header">
+                    <StyledTableCell id="item-name" scope="col" align="center">
                       商品
-                    </TableCell>
-                    <TableCell id="item-size" scope="col" align="center">
+                    </StyledTableCell>
+                    <StyledTableCell id="item-size" scope="col" align="center">
                       サイズ
-                    </TableCell>
-                    <TableCell id="itemPrice" scope="col" align="center">
+                    </StyledTableCell>
+                    <StyledTableCell id="item-price" scope="col" align="center">
                       価格（税抜）
-                    </TableCell>
-                    <TableCell id="itemQuantity" scope="col" align="center">
+                    </StyledTableCell>
+                    <StyledTableCell
+                      id="item-quantity"
+                      scope="col"
+                      align="center"
+                    >
                       数量
-                    </TableCell>
-                    <TableCell id="itemToppingList" scope="col" align="center">
+                    </StyledTableCell>
+                    <StyledTableCell id="item-topping-list" scope="col">
                       トッピング／価格
-                    </TableCell>
-                    <TableCell id="totalPrice" scope="col" align="center">
+                    </StyledTableCell>
+                    <StyledTableCell
+                      id="total-price"
+                      scope="col"
+                      align="center"
+                    >
                       合計
-                    </TableCell>
+                    </StyledTableCell>
                   </TableRow>
-                  <TableRow key={itemIndex}>
-                    <TableRow>
-                      <TableCell headers="item-name">
+                  <StyledTableRow className="per-item-info" key={itemIndex}>
+                    <StyledTableCell>
+                      <div>
                         商品名：{itemInfo.item.name}
                         <br />
                         <img src={itemInfo.item.imagePath} alt="" />
-                      </TableCell>
-                    </TableRow>
-                    <TableCell headers="item-size">({itemInfo.size})</TableCell>
-                    <TableCell>
+                      </div>
+                    </StyledTableCell>
+                    <StyledTableCell headers="item-size">
+                      ({itemInfo.size})
+                    </StyledTableCell>
+                    <>
                       {/* サイズの価格 */}
                       {(() => {
                         if (itemInfo.size === "M") {
                           return (
-                            <TableCell headers="itemPrice">
+                            <StyledTableCell headers="itemPrice">
                               ￥{itemInfo.item.priceM}
-                            </TableCell>
+                            </StyledTableCell>
                           );
                         } else {
                           return (
-                            <TableCell headers="itemPrice">
+                            <StyledTableCell headers="itemPrice">
                               ￥{itemInfo.item.priceL}
-                            </TableCell>
+                            </StyledTableCell>
                           );
                         }
                       })()}
                       {/* end of line  */}
-                    </TableCell>
-                    <TableCell>
-                      <TableCell>{itemInfo.quantity}</TableCell>
-                    </TableCell>
+                    </>
+                    <StyledTableCell>{itemInfo.quantity}</StyledTableCell>
                     {/* orderTopping */}
                     {(() => {
                       if (itemInfo.orderToppingList.length !== 0) {
                         return (
-                          <TableCell className="order-topping">
+                          <StyledTableCell className="order-topping">
                             {itemInfo.orderToppingList.map((topping, index) => (
-                              <TableCell key={index}>
-                                <TableCell>{topping.Topping.name}/</TableCell>
+                              <StyledTableCell
+                                key={index}
+                                sx={{
+                                  display: "flex",
+                                  justifyContent: "space-around",
+                                }}
+                              >
+                                {topping.Topping.name}
                                 {(() => {
                                   if (itemInfo.size === "M") {
                                     return (
-                                      <TableCell>
-                                        ￥{topping.Topping.priceM}
-                                      </TableCell>
+                                      <span>￥{topping.Topping.priceM}</span>
                                     );
                                   } else {
                                     return (
-                                      <TableCell>
-                                        ￥{topping.Topping.priceL}
-                                      </TableCell>
+                                      <span>￥{topping.Topping.priceL}</span>
                                     );
                                   }
                                 })()}
-                              </TableCell>
+                              </StyledTableCell>
                             ))}
-                          </TableCell>
+                          </StyledTableCell>
                         );
                       } else {
                         return (
-                          <TableCell className="order-topping"></TableCell>
+                          <StyledTableCell className="order-topping"></StyledTableCell>
                         );
                       }
                     })()}
-                    <TableCell className="order-total-price">
+                    <StyledTableCell className="order-total-price">
                       ￥{order.totalPrice}
-                    </TableCell>
-                  </TableRow>
+                    </StyledTableCell>
+                  </StyledTableRow>
                 </>
               ))}
-            </TableRow>
+            </StyledTableRow>
           ))}
-        </Table>
-      </TableContainer>
-    </div>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
