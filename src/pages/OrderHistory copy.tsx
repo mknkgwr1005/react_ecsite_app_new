d7d27483d7326ff15f7294172479449b16330d5a
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2024-06-04 10:44:35 +0900

    ページング機能を作成

--- src/pages/OrderHistory copy.tsx
+++ src/pages/OrderHistory copy.tsx
@@ -62,75 +62,70 @@ export const OrderHistory = () => {
    *
    */
   const getOrderHistory = async () => {
-    try {
-      let query = orderData.orderBy("orderDate", "desc");
-      const allProductLength = (await query.get()).docs.length;
-      setOrderLength(allProductLength);
+    // firebaseからすべての注文情報を取得し、クエリを使って並び替える
+    let query = orderData.orderBy("orderDate", "desc");
+    // データの全体の件数を取得する
+    const allProductLength = (await query.get()).docs.length;
+    setOrderLength(allProductLength);
 
-      // ページ1の時だけの処理
-      if (
-        productsPerPage > 0 &&
-        allProductLength > productsPerPage &&
-        currentPage === 1
-      ) {
-        query = query.limit(productsPerPage);
-      }
+    //全体の件数が表示件数を上回った場合、データの数を表示件数に制限する
+    query =
+      productsPerPage > 0 && allProductLength > productsPerPage
+        ? query.limit(productsPerPage)
+        : query;
 
-      // 次のページに移動したときの処理
-      if (currentPage > 1) {
-        const orderData = await query.get();
-        const lastSnapshotIndex = productsPerPage * (currentPage - 1) - 1;
-        console.log("lastSnapshot", lastSnapshotIndex);
-        console.log("orderData", orderData.docs.length);
+    // 現在のページ数が１以上の場合、次のデータを取得する
+    if (currentPage > 1) {
+      const orderData = await query.get();
+      const lastSnapshot = orderData.docs[startFrom * currentPage];
+      const next = query.startAfter(lastSnapshot).limit(productsPerPage);
+      query = next;
+      // レンダリングの問題で二重に表示されるデータを消しました
+      setOrderList(
+        orderList.filter((order, index) => index > productsPerPage - 1)
+      );
+    }
 
-        if (lastSnapshotIndex < orderData.docs.length) {
-          console.log("hi");
-          const lastSnapshot = orderData.docs[lastSnapshotIndex];
-          query = query.startAfter(lastSnapshot).limit(productsPerPage);
-        }
-      }
+    const user = await db
+      .collection("userInformation")
+      .doc(currentUserUid)
+      .get();
+    const userData = user.data();
 
-      // 注文履歴を表示
-      const userSnapshot = await db
-        .collection("userInformation")
-        .doc(currentUserUid)
-        .get();
-      const userData = userSnapshot.data();
-
-      const orderSnapshot = await query.get();
-      const orders = orderSnapshot.docs.map((doc) => {
+    await query.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
         const eachOrder = doc.data();
         const newTimeStamp = new Date(eachOrder.orderDate.toDate());
 
-        return {
-          id: eachOrder.userId,
-          userId: eachOrder.userId,
-          status: eachOrder.status,
-          totalPrice: eachOrder.totalPrice,
-          orderDate: newTimeStamp,
-          destinationName: eachOrder.destinationName,
-          destinationEmail: eachOrder.destinationEmail,
-          destinationZipcode: eachOrder.destinationZipcode,
-          destinationAddress: eachOrder.destinationAddress,
-          destinationTel: eachOrder.destinationTel,
-          deliveryTime: eachOrder.deliveryTime,
-          paymentMethod: eachOrder.paymentMethod,
-          user: {
-            id: userData?.id,
-            name: userData?.name,
-            mailAddress: userData?.mailAddress,
-            password: userData?.password,
-            zipcode: userData?.zipcode,
-            address: userData?.address,
-            telephone: userData?.telephone,
+        setOrderList((prev) => [
+          ...prev,
+          {
+            id: eachOrder.userId,
+            userId: eachOrder.userId,
+            status: eachOrder.status,
+            totalPrice: eachOrder.totalPrice,
+            orderDate: newTimeStamp,
+            destinationName: eachOrder.destinationName,
+            destinationEmail: eachOrder.destinationEmail,
+            destinationZipcode: eachOrder.destinationZipcode,
+            destinationAddress: eachOrder.destinationAddress,
+            destinationTel: eachOrder.destinationTel,
+            deliveryTime: eachOrder.deliveryTime,
+            paymentMethod: eachOrder.paymentMethod,
+            user: {
+              id: userData?.id,
+              name: userData?.name,
+              mailAddress: userData?.mailAddress,
+              password: userData?.password,
+              zipcode: userData?.zipcode,
+              address: userData?.address,
+              telephone: userData?.telephone,
+            },
+            orderItemList: eachOrder.orderItemFormList,
           },
-          orderItemList: eachOrder.orderItemFormList,
-        };
+        ]);
       });
-      setOrderList(orders);
-    } catch (error) {
-      console.error("Error fetching order history:", error);
-    }
+    });
   };
 
   const handleChangePage = (value: number) => {
