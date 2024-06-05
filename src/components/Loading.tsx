9aac8bfa95c2de61049394f6a2e7bbff91c962bf
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2024-06-05 12:42:34 +0900

    注文履歴がない場合の画面を作成

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,14 +2,23 @@ import { Container } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 import "../css/loading.css";
 import { Box } from "@material-ui/core";
+import { NoOrderHistory } from "../components/NoOrderHistory";
 
-export const Loading = () => {
+type loadingType = {
+  noOrderHistory: boolean;
+};
+
+export const Loading = ({ noOrderHistory }: loadingType) => {
   return (
     <Container>
-      <Box className="box">
-        <div>少々お待ちください・・・。</div>
-        <CircularProgress />
-      </Box>
+      {!noOrderHistory ? (
+        <Box className="box">
+          <div>少々お待ちください・・・。</div>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <NoOrderHistory />
+      )}
     </Container>
   );
 };
